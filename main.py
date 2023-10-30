498fa265918c9fc7c26f5fd692c8c8de743f0f1f
wheatgreaser <108815657+wheatgreaser@users.noreply.github.com>
2023-10-30 15:52:02 +0000

    just madr a very crappy chat app while shitting around

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import flask
-from flask import Flask
-
+from flask import Flask, render_template, url_for, request
+import time
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
-def yo():
-  return 'yo'
+time = datetime.now()
+count = 12
+coolList = []
+
+@app.route("/", methods=['GET', 'POST'])
+def coolthing():
+  time = datetime.now()
+  if request.method == 'POST':
+    name = request.form.get('name')
+    coolList.append(name)
+    time = datetime.now()
+  return render_template('index.html', coolList = coolList, count = count, senttime = time)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
